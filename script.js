b1e345f4c691186bb3299ca4cd9f296989cd129f
Guilherme Alberto <gmightasf@gmail.com>
2025-07-06 17:18:37 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function botao(n){
     alert(`Você pressionou o ${n}º botão`)
 }
+
+function exibir_conteudo(id, display_ideal){
+    let alvo = document.getElementById(`${id}`)
+    if(alvo.style.display == 'flex' || alvo.style.display == 'block'){ 
+        alvo.style.display = 'none'
+    }else{
+        alvo.style.display = `${display_ideal}`
+    }
+}
